a8e008fdf15cf0673e3ba50f43ea0ab6555d7c10
Amir Barylko <amir@barylko.com>
2012-01-09 17:05:58 -0600

    First iteration with an average game

--- src/bowling/scenarios/GameSteps.java
+++ src/bowling/scenarios/GameSteps.java
@@ -3,7 +3,11 @@ package bowling.scenarios;
 import static org.hamcrest.CoreMatchers.*;
 import static org.jbehave.Ensure.ensureThat;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.jbehave.scenario.annotations.Given;
+import org.jbehave.scenario.annotations.Named;
 import org.jbehave.scenario.annotations.Then;
 import org.jbehave.scenario.annotations.When;
 import org.jbehave.scenario.steps.Steps;
@@ -25,15 +29,28 @@ public class GameSteps extends Steps {
 	
 	@When("I miss all the pins in every single frame") 
 	public void missEveryFrame() {
-		for(int i = 0; i < 20; i++) {
-			this.game.roll(0);
-		}
+		this.registerRolls(repeat(20, 0));
 	}
 	
 	@When("I do a strike on every frame") 
 	public void scoreEveryFrame() {
-		for(int i = 0; i < 12; i++) {
-			this.game.roll(10);
+		this.registerRolls(repeat(12, 10));
+	}
+	
+	@When("I roll a game of \"$rolls\"")
+	public void rollGame(String rolls) {
+		Collection<Integer> actual = new ArrayList<Integer>();
+		
+		for(String s: rolls.split(",")) {
+			actual.add(Integer.parseInt(s.trim()));
+		}
+		
+		this.registerRolls(actual);
+	}
+	
+	private void registerRolls(Iterable<Integer> rolls) {
+		for(int roll: rolls) {
+			this.game.roll(roll);
 		}
 	}
 	
@@ -42,13 +59,24 @@ public class GameSteps extends Steps {
 		checkScore(0);
 	}
 
-	@Then("the result should be $result")
+	@Then("the score should be the highest")
+	public void checkPerfectScore() {
+		checkScore(300);
+	}
+	
+	@Then("the score is $result")
 	public void checkScore(int result) {
 		ensureThat(this.game.getScore(), is(result));
 	}
 	
-	@Then("the score should be perfect")
-	public void checkPerfectScore() {
-		checkScore(300);
+	private static <T> Iterable<T> repeat(int times, T j) {
+		Collection<T> result = new ArrayList<T>();
+		
+		while(times-- > 0) {
+			result.add(j);
+		}
+		
+		return result;
 	}
+
 }
