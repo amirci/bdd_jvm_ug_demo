869c7e83562b74374fb514ebe72444b91180daa3
Amir Barylko <amir@barylko.com>
2012-01-09 14:52:42 -0600

    Added implementation for steps

--- src/bowling/BowlingGame.java
+++ src/bowling/BowlingGame.java
@@ -2,4 +2,8 @@ package bowling;
 
 public class BowlingGame {
 
+	public int getScore() {
+		return 0;
+	}
+
 }
--- src/bowling/scenarios/GameSteps.java
+++ src/bowling/scenarios/GameSteps.java
@@ -1,10 +1,35 @@
 package bowling.scenarios;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.jbehave.Ensure.ensureThat;
+
+import org.jbehave.scenario.annotations.Given;
+import org.jbehave.scenario.annotations.Then;
+import org.jbehave.scenario.annotations.When;
 import org.jbehave.scenario.steps.Steps;
+
+import bowling.BowlingGame;
 
 public class GameSteps extends Steps {
 
+	private BowlingGame game;
+
 	public GameSteps() {
         super();
+	}
+	
+	@Given("I have a new game")
+	public void setupNewGame() {
+		game = new BowlingGame();
+	}
+	
+	@When("I miss all the pins in every single frame") 
+	public void missEveryFrame() {
+		
+	}
+	
+	@Then("the result should be $result")
+	public void checkTheResult(int result) {
+		ensureThat(this.game.getScore(), is(result));
 	}
 }
